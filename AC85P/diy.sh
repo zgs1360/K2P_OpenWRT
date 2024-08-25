28eac276f7213cb137eca130d2bd5edef97164f9
ztx <30760655@qq.com>
2024-08-25 23:30:18 +0800

    AC85P配置

--- AC85P/diy.sh
+++ AC85P/diy.sh
@@ -1,2 +1,7 @@
 # Modify default IP
 #sed -i 's/192.168.1.1/192.168.2.1/g' package/base-files/files/bin/config_generate
+
+
+#添加科学上网源
+git clone --depth=1 https://github.com/fw876/helloworld package/helloworld
+
